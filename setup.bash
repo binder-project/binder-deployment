e84165383113f5e15fd9dac6d37cae15b8783635
YuviPanda <yuvipanda@gmail.com>
2017-03-09 17:48:13 -0800

    Make kubernetes builder service work properly

--- setup.bash
+++ setup.bash
@@ -1,6 +1,27 @@
 #!/bin/bash
-# Install packages we care about
-sudo apt-get install --yes npm nodejs-legacy
+BINDER_HOME="/var/lib/binder"
+GIT_DIR="${BINDER_HOME}/deploy"
+HOME="/var/lib/binder"
 
-adduser --disabled-password --disabled-login --system --gecos "Binder system user" --home /var/lib/binder binder
-sudo -u binder git clone --recursive https://github.com/yuvipanda/binder-deployment.git /var/lib/binder/deploy
+apt-get install --yes npm nodejs-legacy
+
+sudo wget https://storage.googleapis.com/kubernetes-release/release/v1.5.2/bin/linux/amd64/kubectl -O /usr/local/bin/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+
+adduser --disabled-password --disabled-login --system --gecos "Binder system user" --home ${BINDER_HOME} binder
+sudo -u binder git clone --recursive https://github.com/yuvipanda/binder-deployment.git ${GIT_DIR}
+
+sudo -u binder gcloud container clusters get-credentials binder-cluster-dev --zone=us-central1-b
+# Run npm install
+
+cd ${GIT_DIR}/web/kubernetes
+sudo -u binder npm install
+
+cd ${GIT_DIR}/web/web
+sudo -u binder npm install
+
+rm -rf ${BINDER_HOME}/.binder
+ln -s ${GIT_DIR}/config ${BINDER_HOME}/.binder
+
+ln -s ${GIT_DIR}/web/kubernetes.service /etc/systemd/system/kubernetes.service
+ln -s ${GIT_DIR}/web/kubectl-proxy.service /etc/systemd/system/kubectl-proxy.service
